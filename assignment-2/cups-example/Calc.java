21d95283f73a099979a85353e08d58b6af87ce14
Grigory Bordyugov <gregbg@gmail.com>
2020-04-15 09:17:30 +0200

    fix formatting

--- assignment-2/cups-example/Calc.java
+++ assignment-2/cups-example/Calc.java
@@ -1,7 +1,7 @@
 import java_cup.runtime.*;
 
 class Main {
-    public static void main(String[] argv) throws Exception{
+    public static void main(String[] argv) throws Exception {
         System.out.println("Please type your arithmethic expression:");
         Parser p = new Parser(new scanner());
         p.parse();
