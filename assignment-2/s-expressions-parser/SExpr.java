563ccfee64c395373f64f27c0774414cb09d981b
Grigory Bordyugov <gregbg@gmail.com>
2020-05-25 10:01:02 +0200

    working on s-exr AST

--- assignment-2/s-expressions-parser/SExpr.java
+++ assignment-2/s-expressions-parser/SExpr.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
 interface SExpr {
     public String toString();
 }
@@ -5,20 +8,24 @@ interface SExpr {
 interface SExprAtom extends SExpr {
 }
 
-interface SExprList extends SExpr {
-}
-
 class SExprSymbol implements SExprAtom {
     private String value;
     SExprSymbol(String s) {
         value = s;
     }
+    public String toString() {
+        return "SExprSymbol(" + value + ")";
+    }
+
 }
 
 class SExprString implements SExprAtom {
     private String value;
     SExprString(String s) {
         value = s;
+    }
+    public String toString() {
+        return "SExprSymbol(" + value + ")";
     }
 }
 
@@ -27,6 +34,9 @@ class SExprInt implements SExprAtom {
     SExprInt(Integer i) {
         value = i;
     }
+    public String toString() {
+        return "SExprInt(" + value + ")";
+    }
 }
 
 class SExprDouble implements SExprAtom {
@@ -34,4 +44,21 @@ class SExprDouble implements SExprAtom {
     SExprDouble(Double d) {
         value = d;
     }
+    public String toString() {
+        return "SExprDouble(" + value + ")";
+    }
 }
+
+class SExprList implements SExpr {
+    private List<SExpr> values;
+    SExprList(List<SExpr> vals) {
+        values = vals;
+    }
+    public String toString() {
+        List<String> list = values.stream()
+            .map(elt -> elt.toString())
+            .collect(Collectors.toList());
+        return String.join(", ", list);
+    }
+}
+
